5cfc82e64ce30e78b29d025dcfb703eb85e43408
asmovic <owoyemifood@gmail.com>
2019-09-24 17:00:33 +0100

    gXVX

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,7 @@
 module.exports = {
-
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    cookiekey: process.env.COOKIE_KEY,
+    mongoURI: process.env.MONGO_URI
+    /* mongoURI: 'mongodb+srv://fullstack:fullstack@cluster0-oebsm.mongodb.net/test?retryWrites=true&w=majority' */
 }

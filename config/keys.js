768037cec0e048fb972208cb8a52942e320f9244
asmovic <owoyemifood@gmail.com>
2019-09-24 17:32:05 +0100

    sdfhghj

--- config/keys.js
+++ config/keys.js
@@ -1,13 +1,6 @@
-/* if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod')
 } else {
     module.exports = require('./dev')
-} */
-
-module.exports = {
-    googleClientID: '973795023669-o1p2i2qu227hfka2oeoba4h991bnpj3e.apps.googleusercontent.com',
-    googleClientSecret: 'S4nXTii72U9jBS1e1GKBFOGy',
-    cookiekey: 'bdbjnfjdfbdnbcgkbncfbnkdfcinkcjdli',
-    mongoURI: 'mongodb://fullstack:fullstack@cluster0-shard-00-00-oebsm.mongodb.net:27017,cluster0-shard-00-01-oebsm.mongodb.net:27017,cluster0-shard-00-02-oebsm.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
-    /* mongoURI: 'mongodb+srv://fullstack:fullstack@cluster0-oebsm.mongodb.net/test?retryWrites=true&w=majority' */
 }
+
